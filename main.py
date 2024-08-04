1d73849df642f607477b80e3056337240fece812
Vishy Gopal <112359514+VG-Fish@users.noreply.github.com>
2024-08-03 23:27:28 -0500

    changed main page

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ def make_file_import():
 def unable_to_parse_link():
     return Card(
         make_file_import(),
-        header=A("Home", href="/"),
+        header=A("Home", href="/", ),
         footer=P("Unable to parse link. Make sure it's a valid youtube link.")
     )
 
@@ -27,7 +27,7 @@ def successully_uploaded_video():
     return Titled(
         Card(
             make_file_import(),
-            #header=A("Home", href="/"),
+            header=A("Home", href="/" ),
             id="vid-container"
         )
     )
@@ -39,19 +39,14 @@ def stringify_captions_file(id):
 def get():
     return (
         Title('Video ASL Converter'),
-        Div(H1('Welcome to Video ASL Converter!', style='text-align: center; margin-top: 15px; margin-bottom: 25px;')),
-        Div(A('Home', href='/', style='text-decoration: none; font-size: 25px; margin-left: 20px; margin-right: 20px;'), A('Converter', href='/convertor', style='text-decoration: none; font-size: 25px; margin-left: 20px; margin-right: 20px;'), style='background-color: white; display: flex; justify-content: center; align-items: center; height: 80px; margin-bottom: 25px;'), 
-        Div(H2('What Is Video ASL Converter?', style='text-align: center;'),
-        P('Video ASL Converter is a project designed with the goal of making content accessible for the Deaf and Hard-of-Hearing community. Our platform takes your video and analyzes its components to generate a translation of the video in ASL. Whether it be a presentation, tutorial, or a form of entertainment, our platform ensures that your content is accessible to everyone. Join us as we attempt to overcome and solve barriers to communication and accessability worldwide.'),
-        style='margin-bottom: 25px; text-align: center;'),
-        Div(H2('How to Use Video ASL Converter:'),
-        H4('Step 1: Copy the link to your selected YouTube video.'),
-        H4('Step 2: Go to the ', A('converter', href='/convertor'), 'and paste the url of the video.'),
-        H4('Step 3: Submit the link and voila! Your translated video should be ready shortly.'),
-        style='margin-bottom: 25px; text-align: center;'),
-        Div(H2('About Us'),
-        P('Video ASL Converter was created by a group of three high-school students with the goal of bridging gaps in communication and accessibility globally. We recognized the need for greater accessibility that comes with the global growth of media, and decided to develop a solution. Our work was fueled by a belief that access to information should be universal. Video ASL Converter was created as a means of increasing accessibility by making content more accessible to the Deaf community via converting speech to ASL.'),
-        style='margin-bottom: 25px; text-align: center;')
+        Div(H2('Welcome to Video ASL Converter!', style='text-align: center; font: small-caps bold 24px/1 sans-serif; color: #FF4500;')),
+        Div(H3('What Is Video ASL Converter?', style='text-align: center; font: small-caps  24px/1 sans-serif; color:#FFD700 '),
+        P('Video ASL Converter is a project designed with the goal of making content accessible for the Deaf and Hard-of-Hearing community. Our platform takes your video and analyzes its components to generate a translation of the video in ASL. Whether it be a presentation, tutorial, or a form of entertainment, our platform ensures that your content is accessible to everyone. Join us as we attempt to overcome and solve barriers to communication and accessability worldwide.', style = 'color: #FFF5EE;')),
+        Div(H3('How to Use Video ASL Converter:', style = 'font: small-caps bold 24px/1 sans-serif; color: #FFD700'),
+        H4('Step 1: Copy the link to your selected YouTube video.', style = 'color: #FFF5EE;'),
+        H4('Step 2: Click this', A('link', href='/convertor'), 'and paste the url of the video.', style = 'color: #FFF5EE;')),
+        Div(H3('About Us',style = 'font: small-caps bold 24px/1 sans-serif; color: #FFD700; '),
+        P('Video ASL Converter was created by a group of three high-school students with the goal of bridging gaps in communication and accessibility globally. We recognized the need for greater accessibility that comes with the global growth of media, and decided to develop a solution. Our work was fueled by a belief that access to information should be universal. Video ASL Converter was created as a means of increasing accessibility by making content more accessible to the Deaf community via converting speech to ASL.', style = 'color: #FFF5EE;'))
     )
 
 @rt("/convertor")
